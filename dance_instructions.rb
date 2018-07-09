9b4911bcd0fe129e8353349763c2e25ccebbe54f
marissa-a <marissa.andrade@verizon.net>
2018-07-09 23:33:14 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -7,7 +7,11 @@ end
 def Base_footwork
   puts "Lift right foot"
   puts "Return right foot to the ground"
-  puts "Finishing with a small skip-step"
+  puts "Finishing with a Small_skip_step"
 end
 
-def Small_skip_step
+def Small_skip_step_left
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Left,Right,Left and Left"
+  
