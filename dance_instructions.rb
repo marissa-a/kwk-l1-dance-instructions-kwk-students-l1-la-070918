533465809bb6624a1e852f1ca895eb0095122e9c
marissa-a <marissa.andrade@verizon.net>
2018-07-09 23:28:59 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,4 +1,13 @@
 
 # Code your methods below
-def Lower_body
-  puts "Plant legs far apart, bend "
+def Assume_the_proper_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
+
+def Base_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip-step"
+end
+
+def skip-step
